00d80107527966776db93b9df91bb3fe32483c25
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-16 01:10:57 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors()); // use to access server if not then cors error will given
-app.use(express.json()); // after getting data as json string convert to js object
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjl9nwy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
   res.send("Doctors portal server is running");
