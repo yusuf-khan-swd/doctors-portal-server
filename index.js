c0e59395fa910a6f6b55eebc95cc21447a15623f
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-17 01:28:27 +0600

    added v2 aggregate and pipeline

--- index.js
+++ index.js
@@ -35,6 +35,52 @@ async function run() {
       res.send(options);
     });
 
+    app.get('/v2/appointmentOptions', async (req, res) => {
+      const date = req.query.date;
+      const options = await appointmentOptionCollection.aggregate([
+        {
+          $lookup: {
+            from: 'bookings',
+            localField: 'name',
+            foreignField: 'treatment',
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $eq: ['$appointmentDate', date]
+                  }
+                }
+              }
+            ],
+            as: 'booked'
+          }
+        },
+        {
+          $project: {
+            name: 1,
+            slots: 1,
+            booked: {
+              $map: {
+                input: "$booked",
+                as: 'book',
+                in: '$$book.slot'
+              }
+            }
+          }
+        },
+        {
+          $project: {
+            name: 1,
+            slots: {
+              $setDifference: ['$slots', '$booked']
+            }
+          }
+        }
+      ]).toArray();
+
+      res.send(options);
+    });
+
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
       const result = await bookingsCollection.insertOne(booking);
