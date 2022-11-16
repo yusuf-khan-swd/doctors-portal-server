850d425fe8a9b3a6c8752839bc79e0ba59b4f5d7
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-16 19:33:23 +0600

    get the all appointment and post a booking object

--- index.js
+++ index.js
@@ -12,6 +12,32 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjl9nwy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+  try {
+    const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+    const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+
+    app.get('/appointmentOptions', async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+  }
+  finally {
+
+  }
+
+}
+
+run().catch(console.log)
+
 app.get('/', (req, res) => {
   res.send("Doctors portal server is running");
 });
