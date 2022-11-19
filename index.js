872d1b3cddb80a31d8113f3a4bb5f2d26ba1847b
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-19 11:44:18 +0600

    create a api for get all users from db

--- index.js
+++ index.js
@@ -159,6 +159,12 @@ async function run() {
       res.status(403).send({ accessToken: '' });
     });
 
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
