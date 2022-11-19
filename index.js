b043aa074364ec9f568c6a21f3b7f4a02d25be7f
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-19 21:10:24 +0600

    get special option collection

--- index.js
+++ index.js
@@ -116,6 +116,12 @@ async function run() {
       res.send(options);
     });
 
+    app.get("/appointmentSpecialty", async (req, res) => {
+      const query = {};
+      const result = await appointmentOptionCollection.find(query).project({ name: 1 }).toArray();
+      res.send(result);
+    });
+
     app.get('/bookings', verifyJWT, async (req, res) => {
       const email = req.query.email;
       const decodeEmail = req.decode.email;
