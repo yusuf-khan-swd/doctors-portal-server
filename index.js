96a6d2c1247292be85849f3382ce2ff0001a50dd
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-21 14:25:39 +0600

    added stripe payment method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -176,6 +177,24 @@ async function run() {
 
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        "payment_method_types": [
+          "card"
+        ],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     app.get('/jwt', async (req, res) => {
